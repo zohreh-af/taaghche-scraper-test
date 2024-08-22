089b646095a7a9af70edcabdf07187bedff79e00
zohreh <abbasifar.zohreh@gmail.com>
2024-08-23 02:09:19 +0330

    output json

--- first/first/spiders/crawling_spider.py
+++ first/first/spiders/crawling_spider.py
@@ -6,5 +6,13 @@ class CrawlingSpider(CrawlSpider):
     allowed_domins = ["taaghche.com"]
     start_urls = ["https://taaghche.com/"]
     rules = (
-        Rule(LinkExtractor(allow=('/category/',)), callback='parse_item', follow=True),
-    ).header_container__xofsz
+        Rule(LinkExtractor(allow=('/category/',)), callback='parse_item',),
+    #    Rule(LinkExtractor(allow=('/category/%DA%A9%D8%AA%D8%A7%D8%A8-%D8%B1%D9%85%D8%A7%D9%86/')),)
+    )
+    def pars_item(self,response):
+        yield{
+            "title": response.css(".bookCard_bookTitle__ELp4O div::text").get(),
+            "auther": response.css(".bookCard_bookAuthor__myAZB::text").get(),
+            "price": response.css(".bookCard_price__zOAwb::text").get()
+
+        }
